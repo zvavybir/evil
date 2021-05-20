b576d7e3fbf42f8443b2e38ac20880ca0bea969c
Matthias Kaak <m.kaak@protonmail.ch>
2021-05-20 18:09:56 +0000

    fixed copy/paste mistake

--- evilsha/src/lib.rs
+++ evilsha/src/lib.rs
@@ -46,7 +46,7 @@
 
 //! Unsecure implementation of SHA 2 in pure rust
 //!
-//! evilaes is a implementation of SHA 2 written by an amateur
+//! evilsha is a implementation of SHA 2 written by an amateur
 //! for the sole purpose that he learns a bit about cryptography.  It
 //! is very probably *very* vulnerable, so **do not use evilsha**.
 //! The same applies to all other evil\* crates.
